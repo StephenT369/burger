6a0e7edb8e86d00cc3f564cfd349d2bb3f7adb30
Stephen Thompson <smt3@hotmail.com>
2019-09-22 22:28:59 -0500

    inital

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+var connecton = require ('../config/connection.js');
+var orm = {
+    selectAll: function(){
+
+    },
+    insertOne: function(){
+
+    },
+    updateOne: function(){
+
+    }
+};
+
+module.exports = orm;
