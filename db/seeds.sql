a657d747728b1ba8af1506434e59eee4de71b066
Stephen Thompson <smt3@hotmail.com>
2019-09-22 22:28:38 -0500

    inital

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured)
+	VALUES ('Glam Burger', false);
+INSERT INTO burgers (burger_name, devoured)
+	VALUES ('Black Bean Burger', true);
+INSERT INTO burgers  (burger_name, devoured)
+	VALUES ('Veggie Burger', true);
