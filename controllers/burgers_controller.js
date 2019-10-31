dfdccffcfa8bea02aba2075f3d76f5b9903c6b30
Stephen Thompson <smt3@hotmail.com>
2019-10-31 15:11:18 -0500

    controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,30 @@
 var express = require('express');
-var burger = require('../models/burger.js');
+var hamburger = require('../models/hamburger.js');
 
-var router = express.Router();
+var hamburgerRtr = express.Router();
 
+hamburgerRtr.get("/", function(req, res) {
+  res.redirect("/hamburgers");
+});
 
+hamburgerRtr.get("/hamburgers", function(req, res) {
+  hamburger.all(function(burgerData) {
+    res.render("index", { burger_data: burgerData });
+  });
+});
 
-module.exports = router;
+hamburgerRtr.post("/hamburgers/create", function(req, res) {
+  hamburger.create(req.body.burger_name, function(result) {
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+hamburgerRtr.put("/hamburgers/:id", function(req, res) {
+  hamburger.update(req.params.id, function(result) {
+    console.log(result);
+    res.sendStatus(200);
+  });
+});
+
+module.exports = hamburgerRtr;
