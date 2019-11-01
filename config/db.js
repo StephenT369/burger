a7a4eeb4121cf4713abde132ee750f17d52e0878
Stephen Thompson <smt3@hotmail.com>
2019-10-31 21:01:50 -0500

    updated db connection

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-var db = mysql.createConnection({
+var db
+if (process.env.JAWSDB_URL){
+  db = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+db = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "SMUb00tc@mp369",
   database: "burgers_db"
 });
+};
+
 
 // Make connection.
 db.connect(function(err) {
